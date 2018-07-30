a507ff051870a7e8660da8fb06360cbb6be2917e
Bill Doody <wdoody@yahoo.com>
2018-07-30 14:21:59 -0600

    framework

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Header = () => {
+    return (
+        <View>
+            <Text>Crypto Portfolio</Text>
+        </View>
+    )
+}
+ export default Header;
